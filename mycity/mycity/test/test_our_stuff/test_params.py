02eef95b77ce962fd8e39e32f9fb58fa511ca1bc
ethanstrominger <ethanstrominger2@gmail.com>
2019-02-13 23:11:09 -0500

    Added distance as a parameter to ArcGisParams __init__

--- mycity/mycity/test/test_our_stuff/test_params.py
+++ mycity/mycity/test/test_our_stuff/test_params.py
@@ -1,16 +1,17 @@
 import unittest
 
-from mycity.test.test_our_stuff.test_long_lat import LongLatPoint
+from mycity.mycity.test.test_our_stuff.test_long_lat import LongLatPoint
 
 # TODO: Remove duplication, also in test_grocery_store_intent, 4326 duplicated
 # TODO: Add distance and outfields as optional parameters - to be decided if part of __init__ or not
+from mycity.mycity.test.test_our_stuff.test_distance import Distance, Mile
 
 
 class ArcGisParams(object):
     _LAT_LONG_SPATIAL_REFERENCE = 4326
     _MILE_UNIT = "esriSRUnit_StatuteMile"
 
-    def __init__(self, origin: LongLatPoint):
+    def __init__(self, origin: LongLatPoint, distance: Distance = Mile(0.001)):
         self.origin = origin
         self.url_param = {
             "f": "json",
@@ -19,7 +20,7 @@ class ArcGisParams(object):
             "geometryType": "esriGeometryPoint",
             "returnGeometry": "false",
             "outFields": "*",
-            "distance": 0.001,
+            "distance": distance.mile,
             "units": self._MILE_UNIT
         }
 
@@ -47,5 +48,23 @@ class TestParams(unittest.TestCase):
             "units": "esriSRUnit_StatuteMile"
         }
         params = ArcGisParams(origin)
+        actual_params = params.url_params()
+        self.assertEqual(expected_params, actual_params)
+
+    def test_distance_param(self):
+        origin = LongLatPoint(30.0, 45.0)
+        miles = 1.78
+        expected_params = {
+            "f": "json",
+            "inSR": 4326,
+            "geometry": f"{origin.x},{origin.y}",
+            "geometryType": "esriGeometryPoint",
+            "returnGeometry": "false",
+            "outFields": "*",
+            "distance": miles,
+            "units": "esriSRUnit_StatuteMile"
+        }
+        distance = Mile(miles)
+        params = ArcGisParams(origin, distance)
         actual_params = params.url_params()
         self.assertEqual(expected_params, actual_params)
